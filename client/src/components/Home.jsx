7f826a02a0e16db249bdc91c5767691c0e60fd37
Muttyfaw <106316623+Muttyfaw@users.noreply.github.com>
2023-11-17 16:22:44 +0000

    sucessfully connected virtual db to backend

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home(){
+    return <div>
+       <header>
+          <div>Rendered</div>
+       </header>
+    </div>
+};
+
+export default Home;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,24 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
+const port = process.env.PORT 
+const mongoose = require("mongoose")
 
+
+
+mongoose.connect(process.env.DATABASE,{
+    serverSelectionTimeoutMS: 5000
+}).then(()=>console.log("Sucessfully connected to DB"))
+
+app.route('/')
+
+.get((req, res)=>{
+    req.send ()
+})
+
+app.listen(port , ()=>{
+    console.log("Server has started on port", port)
+    
+})
+ 
+ 

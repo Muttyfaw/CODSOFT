93e238b3cef804cd9f8f3f5b8be28b8e4766e755
Muttyfaw <106316623+Muttyfaw@users.noreply.github.com>
2023-11-30 21:23:25 +0000

    added the avatar tag again

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -128,7 +128,7 @@ const Navbar =()=> {
                     <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title="Open settings">
                             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                {/* <Avatar alt="" src="" /> */}
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
                             </IconButton>
                         </Tooltip>
                         <Menu
